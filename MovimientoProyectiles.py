3da530be443a196c493a532048645cdc23b84e14
mauricioara <mauricioher.94@hotmail.com>
2017-04-16 18:54:51 -0600

    arrow

--- MovimientoProyectiles.py
+++ MovimientoProyectiles.py
@@ -12,7 +12,7 @@ import sys
 #Variables pre definidas
 #########################################
 #Angulo de disparo
-angulo_de_disparo = 65
+angulo_de_disparo = 45
 #Velocidad inicial
 velocidad_inicial = 9
 #########################################
@@ -20,8 +20,8 @@ velocidad_inicial = 9
 #Variables Globales
 ###############
 gravedad = 9.8
-dt = 0.01
-altura_inicial = -2
+dt = 0.005
+altura_inicial = 0
 tiempo_total_vuelo = (2 * velocidad_inicial * math.sin(math.radians(angulo_de_disparo))) / gravedad
 distancia_proyectil = velocidad_inicial * math.cos(math.radians(angulo_de_disparo)) * tiempo_total_vuelo
 ###############
@@ -36,25 +36,33 @@ def iniciar_mundo():
      x=0, y=0, width=900, height=600,
      center=(5,0,0), background=(0,1,1))
 
-    arbol = cylinder(pos=(2.3,0.1,3), axis=(0,1,0), radius=0.1, material=materials.wood)
-    hojas_arbol = sphere(pos=(2.3,1,3), axis=(0.5,0.5,3), radius=0.4)
-    hojas_arbol.color=(0,1,0)
+    #arbol = cylinder(pos=(2.3,0.1,3), axis=(0,1,0), radius=0.1, material=materials.wood)
+    #hojas_arbol = sphere(pos=(2.3,1,3), axis=(0.5,0.5,3), radius=0.4)
+    #hojas_arbol.color=(0,1,0)
 
-    arbol2 = cylinder(pos=(3,0.1,3), axis=(0,1,0), radius=0.1,  material=materials.wood)
-    hojas_arbol2 = sphere(pos=(3,1,3), axis=(0.5,0.5,3), radius=0.4)
-    hojas_arbol2.color=(0,1,0)
+    #arbol2 = cylinder(pos=(3,0.1,3), axis=(0,1,0), radius=0.1,  material=materials.wood)
+    #hojas_arbol2 = sphere(pos=(3,1,3), axis=(0.5,0.5,3), radius=0.4)
+    #hojas_arbol2.color=(0,1,0)
 
-    arbol3 = cylinder(pos=(5,-2,3), axis=(0,1,0), radius=0.1, material=materials.wood)
-    hojas_arbol3 = sphere(pos=(5,-1,3), axis=(0.5,0.5,3), radius=0.4)
-    hojas_arbol3.color=(0,1,0)
+    #arbol3 = cylinder(pos=(5,-2,3), axis=(0,1,0), radius=0.1, material=materials.wood)
+    #hojas_arbol3 = sphere(pos=(5,-1,3), axis=(0.5,0.5,3), radius=0.4)
+    #hojas_arbol3.color=(0,1,0)
 
-    sol = sphere(pos=(8,2.5,1), axis=(0.5,0.5,3), radius=0.3)
-    sol.color=(1,1,0)
+    #sol = sphere(pos=(8,2.5,1), axis=(0.5,0.5,3), radius=0.3)
+    #sol.color=(1,1,0)
 
     base = box(pos=(0,altura_inicial,1), height=0.1, width=0.5, material=materials.wood)
     base.color=(1,0.9,0.3)
 
     cannon = cylinder(pos=(0,altura_inicial,1), radius=0.1)
+
+
+    tiempoY = (velocidad_inicial * math.sin(math.radians(angulo_de_disparo))) / gravedad
+    posicionY = (velocidad_inicial * math.sin(math.radians(angulo_de_disparo))) * tiempoY - 0.5 * gravedad * math.pow(tiempoY, 2)
+    posicionX = velocidad_inicial * math.cos(math.radians(angulo_de_disparo)) * tiempoY
+
+    
+    highestPointArrow = arrow(pos=(posicionX,posicionY + 5, 0), axis=(0,-5,0), shaftwidth=0.2, color=color.yellow)
 
     bola_cannon = sphere(pos=(0,altura_inicial,1),radius=0.1,
     make_trail = true, material=materials.chrome, opacity=0.5)
@@ -80,11 +88,11 @@ def iniciar_mundo():
     t=0
     bola_en_aire = True
     flag = False
-    while bola_en_aire:
-        rate(50)
+    while t <= tiempo_total_vuelo:
+        rate(100)
         bola_cannon.pos = (velocidad_inicial*cos(math.radians(angulo_de_disparo))*t,
                             altura_inicial + velocidad_inicial*t*sin(math.radians(angulo_de_disparo)) -0.5 * gravedad *t**2)
-
+        
         if bola_cannon.y < -2: #El proyectil llega al suelo
             print "bola_cannon.pos = ",bola_cannon.pos,"t = ",t
             bola_en_aire = False
